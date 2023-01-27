820b70dfd7d172783a7c04ea63f8b9aee8d8b229
Nora <nsorym04@gmail.com>
2023-01-28 01:30:33 +0900

    adobe fontの読み込み

--- alma/src/pages/_app.tsx
+++ alma/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import adobeLoader from "../../adobeLoader";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.browser) adobeLoader(document);
+  }, [])
+    
   return(
     <ChakraProvider>
       <Component {...pageProps} />
