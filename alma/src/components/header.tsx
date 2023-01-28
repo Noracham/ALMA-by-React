eb19de7489ac5c752fe859de60e86530c2b174a0
Nora <nsorym04@gmail.com>
2023-01-28 13:17:02 +0900

    header component作成

---
+++ alma/src/components/header.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import styles from "@/styles/Header.module.css";
+
+export const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ALMA</h1>
+      </header>
+    </>
+  );
+};
